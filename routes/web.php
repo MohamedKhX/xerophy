4f986bb7e48ce34c987b5bd94653323729c44718
MohamedKhX <muhamedkhx2@gmail.com>
2022-05-11 20:34:55 +0200

    add the project files

---
+++ routes/web.php
@@ -0,0 +1,21 @@
+<?php
+
+use Xerophy\Framework\Application\Application;
+use Xerophy\Framework\Routing\Router;
+
+
+/**.
+ *
+ * -------- Get the router from the application --------
+ *
+ * @var $router Router
+ * @var $app Application
+ * */
+$router = $app->getRouter();
+
+/**
+ * -------- Start register your routes here (: --------
+ */
+
+$router->view('/', 'index.html.twig');
+
